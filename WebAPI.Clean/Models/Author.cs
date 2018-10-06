0409e698c56d4ef8425a3bd6e3f273f5b8385dd4
Max Chou <max9159@gmail.com>
2018-10-06 17:14:59 +0800

    add controler, model, binding (code first)

---
+++ WebAPI.Clean/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Clean.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
