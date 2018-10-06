21ac7d80e27213fec8db997de3eb8b316d0d3955
Max Chou <max9159@gmail.com>
2018-10-06 15:31:55 +0800

    add partial model

--- WebAPI.Skilltree.vs17/Models/Partials/Product.cs
+++ WebAPI.Skilltree.vs17/Models/Partials/Product.cs
@@ -5,40 +5,30 @@ namespace WebAPI.Skilltree.vs17.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using Newtonsoft.Json;
 
+    [MetadataType(typeof(ProductMD))]
     public partial class Product
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Product()
+        public class ProductMD
         {
-            Order_Details = new HashSet<Order_Detail>();
+            public int ProductID { get; set; }
+            [Required]
+            [StringLength(40)]
+            public string ProductName { get; set; }
+            public int? SupplierID { get; set; }
+            public int? CategoryID { get; set; }
+            [StringLength(20)]
+            public string QuantityPerUnit { get; set; }
+            [Column(TypeName = "money")]
+            public decimal? UnitPrice { get; set; }
+            public short? UnitsInStock { get; set; }
+            public short? UnitsOnOrder { get; set; }
+            public short? ReorderLevel { get; set; }
+            public bool Discontinued { get; set; }
+
+            [JsonIgnore]
+            public virtual ICollection<Order_Detail> Order_Details { get; set; }
         }
-
-        public int ProductID { get; set; }
-
-        [Required]
-        [StringLength(40)]
-        public string ProductName { get; set; }
-
-        public int? SupplierID { get; set; }
-
-        public int? CategoryID { get; set; }
-
-        [StringLength(20)]
-        public string QuantityPerUnit { get; set; }
-
-        [Column(TypeName = "money")]
-        public decimal? UnitPrice { get; set; }
-
-        public short? UnitsInStock { get; set; }
-
-        public short? UnitsOnOrder { get; set; }
-
-        public short? ReorderLevel { get; set; }
-
-        public bool Discontinued { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Order_Detail> Order_Details { get; set; }
     }
 }
