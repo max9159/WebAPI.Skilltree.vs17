c3b7b1cccdfbf85f52cf469f80e04d63ba2ae939
Max Chou <max9159@gmail.com>
2018-10-13 15:55:47 +0800

    Set up HelpPage for APIs Install-Package Microsoft.AspNet.WebApi.HelpPage

--- WebAPI.Skilltree.vs17/Global.asax.cs
+++ WebAPI.Skilltree.vs17/Global.asax.cs
@@ -13,7 +13,7 @@ namespace WebAPI.Skilltree.vs17
 	{
 		protected void Application_Start()
 		{
-			AreaRegistration.RegisterAllAreas();
+			AreaRegistration.RegisterAllAreas();//for helper page
 			GlobalConfiguration.Configure(WebApiConfig.Register);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
