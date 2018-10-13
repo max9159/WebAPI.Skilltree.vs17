e23a2d25585dd9b2c5c81c6b8b6c74ede387bcc7
Max Chou <max9159@gmail.com>
2018-10-13 15:15:57 +0800

    Add model validation control in ActionExecuting

--- WebAPI.Skilltree.vs17/App_Start/WebApiConfig.cs
+++ WebAPI.Skilltree.vs17/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using WebAPI.Skilltree.vs17.Filters;
 using WebAPI.Skilltree.vs17.Forrmatters;
 
 namespace WebAPI.Skilltree.vs17
@@ -11,6 +12,8 @@ namespace WebAPI.Skilltree.vs17
 		public static void Register(HttpConfiguration config)
 		{
             // Web API configuration and services
+            config.Filters.Add(new ModelValidationFilterAttribute());
+
             config.Formatters.Add(new ProductCsvFormatter());
 			// Web API routes
 			config.MapHttpAttributeRoutes();
---
+++ WebAPI.Skilltree.vs17/Filters/ModelValidationFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebAPI.Skilltree.vs17.Filters
+{
+    public class ModelValidationFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            // 在回應本體回傳驗證錯誤訊息
+            if (actionContext.ModelState.IsValid == false)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(
+                    HttpStatusCode.BadRequest, actionContext.ModelState);
+            }
+        }
+    }
+}
