761ac3da1e74b086b9a54c3800205328820d6c71
Max Chou <max9159@gmail.com>
2018-10-20 11:40:06 +0800

    Install-Package elmah, try to build get error log

---
+++ WebAPI.Skilltree.vs17/Controllers/ElmahController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Skilltree.vs17.Controllers
+{
+    public class ElmahController : ApiController
+    {
+        [HttpGet]
+        [Route("tryElmah/{id:int}")]
+        public int tryElmah(int id)
+        {
+            throw new NotImplementedException();
+
+            if (id > 10)
+            {
+                throw new Exception("From: throw new Exception");
+            }
+            return id;
+        }
+    }
+}
